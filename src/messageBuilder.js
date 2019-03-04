a4fe71d8e74cf708eed8df697e06d5982ff47dd5
komamiya <pringles.oji3+dev@gmail.com>
2019-03-04 15:00:55 +0900

    button修正

--- src/messageBuilder.js
+++ src/messageBuilder.js
@@ -6,14 +6,16 @@ export function createLink (text, url) {
   }
 }
 export function createExternalLinks (code) {
-  const attachments = []
+  const actions = []
 
-  attachments.push(createLink('株探', `https://kabutan.jp/stock/chart?code=${code}`))
-  attachments.push(createLink('株ライン', `https://kabuline.com/stock/code/${code}/`))
+  actions.push(createLink('株探', `https://kabutan.jp/stock/chart?code=${code}`))
+  actions.push(createLink('株ライン', `https://kabuline.com/stock/code/${code}/`))
   // return createLink('https://kabutan.jp/favicon.ico', 'https://kabutan.jp/stock/chart?code=' + code)
 
   return {
     text: `外部リンク ${code}`,
-    attachments
+    attachments: [{
+      actions
+    }]
   }
 }
