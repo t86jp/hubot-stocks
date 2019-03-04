52e1b4fb20b435b322c9a9131405fd9dce332e42
komamiya <pringles.oji3+dev@gmail.com>
2019-03-04 10:54:53 +0900

    銘柄code取得を調整

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Kabutan from './kabutan'
+import R from 'ramda'
 const childProcess = require('child_process')
 const moji = require('moji')
 require('dotenv').config()
@@ -6,6 +7,24 @@ require('dotenv').config()
 const CHART_DIR = './tmp'
 const STOCK_NUMBER_REGEX = /([0-9０-９]{4})/g
 
+function * findStockNumber (message) {
+  let _message = message
+  const noise = (matcher) => _message = _message.replace(matcher, '')
+
+  // March 04, 2019
+  noise(/(?:Jan(?:uary|\.)|Feb(?:ruary|\.)|Mar(?:ch|\.)|Apr(?:il|\.)|May|Jun(?:e|\.)|Jul(?:y|\.)|Aug(?:ust|\.)|Sep(?:tember|\.)|Oct(?:ober|\.)|Nov(?:ember|\.)|Dec(?:ember|\.))\s+[0-3][0-9],\s+20[0-9][0-9]/gi)
+  // at 10:16AM
+  noise(/\s+at\s*[0-9]{2}:[0-9]{2}(?:am|pm)?/gi)
+  // url
+  noise(/https?:\/\/[^ ]+/g)
+  // xxxx円
+  noise(/[0-9０-９]+円/g)
+
+  let matches = (_message.match(STOCK_NUMBER_REGEX) || []).map((number) => normalizeStockNumber(number))
+  for (let number of R.uniqWith(R.eqBy(String))(matches)) {
+    yield number
+  }
+}
 const normalizeStockNumber = (stockNumber) => {
   return moji(stockNumber).convert('ZE', 'HE').toString()
 }
@@ -14,24 +33,28 @@ function saveChartImage (stockNumber) {
 
   return new Promise((resolve, reject) => {
     let kabutan = new Kabutan()
-    kabutan.openPageByCode(stockNumber, async (page) => {
-      let rect = await page.evaluate(function () {
-        document.getElementById('kc_ashi_4').click()
-        document.getElementById('kc_tech2_11').click()
+    kabutan.openPageByCode(stockNumber, (page) => {
+      let callbackPromise = new Promise((callbackResolve, callbackReject) => {
+        let interval = setTimeout(() => {
+          page.evaluate(function () {
+            document.getElementById('kc_ashi_4').click()
+            document.getElementById('kc_tech2_11').click()
 
-        return document.getElementById('kc_area').getBoundingClientRect()
+            return document.getElementById('kc_area').getBoundingClientRect()
+          }).then((rect) => {
+            kabutan.clipRect({
+              width: rect.width,
+              height: rect.height,
+              top: rect.top,
+              left: rect.left
+            })
+
+            page.render(filename).then(() => callbackResolve(filename))
+          }).catch((e) => callbackReject(e))
+        }, 500)
       })
 
-      kabutan.clipRect({
-        width: rect.width,
-        height: rect.height,
-        top: rect.top,
-        left: rect.left
-      })
-
-      await page.render(filename)
-
-      resolve(filename)
+      return callbackPromise.then(() => resolve(filename)).catch((e) => reject(e))
     })
   })
 }
@@ -52,9 +75,7 @@ module.exports = (robot) => {
   robot.hear(STOCK_NUMBER_REGEX, async (message) => {
     const channel = message.envelope.room == 'Shell' ? '株bot-test' : message.envelope.room
 
-    message.match.forEach((number) => {
-      let code = normalizeStockNumber(number)
-
+    for (let code of findStockNumber(message.message.text)) {
       try {
         saveChartImage(code)
           .then(async (filename) => {
@@ -64,6 +85,6 @@ module.exports = (robot) => {
       } catch (e) {
         message.send(`error stock=${code}: チャートerror: ${e}`)
       }
-    })
+    }
   })
 }
